847581e1ecc3c740a807895a1c6843e46a795450
Nicolò <63916183+nicolovaldiserri@users.noreply.github.com>
2025-05-11 10:32:53 +0200

    Update code.asm

--- code.asm
+++ code.asm
@@ -3,36 +3,36 @@ init:       LHI		R21,0x8000            ; set R21 = 0x80000000h (command address)
             BEQZ	R22,handler           ; if (STARTUP == 0) then jump to handler
       					      ; begin preamble: perform all startup initialization tasks here beacause interrupts are disabled
       					      ; storage of the incompatibility table
-	    LHI		R23,0x0007            ; set R23 = 0x00070000h       row 1		1-I
-            ADDUI	R1,R23,0xC024         ; set R1  = 0x0007C024h       row 1 		1-I
-            LHI		R23,0x000B            ; set R23 = 0x000B0000h       row 2		1-II
-            ADDUI	R2,R23,0xFFDB         ; set R2  = 0x000BFFDBh       row 2		1-II
-            LHI		R23,0x000D            ; set R23 = 0x000D0000h       row 3		1-III
-            ADDUI	R3,R23,0xFFDB         ; set R3  = 0x000DFFDBh       row 3		1-III
-            LHI		R23,0x000E            ; set R23 = 0x000E0000h       row 4		I-1
-            ADDUI	R4,R23,0xC000         ; set R4  = 0x000EC000h		row 4		I-1
-            LHI		R23,0x000F            ; set R23 = 0x000F0000h		row 5		II-1
-            ADDUI	R5,R23,0x7C00         ; set R5  = 0x000F7C00h		row 5		II-1
-            ADDUI	R6,R23,0xBD08         ; set R6  = 0x000FBD08h		row 6		III-1
-            LHI		R23,0x0006            ; set R23 = 0x00060000h		row 7		2-II
-            ADDUI	R7,R23,0xDFDB         ; set R7  = 0x0006DFDBh		row 7		2-II
-            ADDUI	R8,R23,0xEFDB         ; set R8  = 0x0006EFDBh		row 8		2-III
-            ADDUI	R9,R23,0xF400         ; set R9  = 0x0006F400h		row 9		II-2
-            ADDUI	R10,R23,0xF908        ; set R10 = 0x0006F908h		row 10     III-2
-            ADDUI	R11,R23,0xB9DB        ; set R11 = 0x0006B9DBh		row 11		3-II
-            ADDUI	R12,R23,0x76DB        ; set R12 = 0x000676DBh		row 12		3-III
-            ADDUI	R13,R23,0x135B        ; set R13 = 0x0006135Bh		row 13		II-3
-            ADDUI	R14,R23,0x339B        ; set R14 = 0x0006339Bh		row 14		III-3
-            LHI		R23,0x0009            ; set R21 = 0x00090000h		row 15		4-I
-            ADDUI	R15,R23,0x001F        ; set R15 = 0x0009001Fh		row 15		4-I
-            LHI		R23,0x0006            ; set R21 = 0x00060000h		row 16		4-II
-            ADDUI	R16,R23,0xBBEF        ; set R16 = 0x0006BBEFh		row 16		4-II
-            ADDUI	R17,R23,0x77F7        ; set R17 = 0x000677F7h		row 17		4-III
-            ADDUI	R18,R0,0x003B         ; set R18 = 0x0000003Bh		row 18		I-4
-            LHI		R23,0x0002            ; set R23 = 0x00020000h		row 19		II-4
-            ADDUI	R19,R23,0x13FD        ; set R19 = 0x000213FDh		row 19		II-4
-            LHI		R23,0x0006            ; set R23 = 0x00060000h		row 20		III-4
-            ADDUI	R20,R23,0x33FE        ; set R20 = 0x000633FE		row 20		III-4
+	    LHI		R23,0x0007            ; set R23 = 0x00070000h       row 1      1-I
+            ADDUI	R1,R23,0xC024         ; set R1  = 0x0007C024h       row 1      1-I
+            LHI		R23,0x000B            ; set R23 = 0x000B0000h       row 2      1-II
+            ADDUI	R2,R23,0xFFDB         ; set R2  = 0x000BFFDBh       row 2      1-II
+            LHI		R23,0x000D            ; set R23 = 0x000D0000h       row 3      1-III
+            ADDUI	R3,R23,0xFFDB         ; set R3  = 0x000DFFDBh       row 3      1-III
+            LHI		R23,0x000E            ; set R23 = 0x000E0000h       row 4      I-1
+            ADDUI	R4,R23,0xC000         ; set R4  = 0x000EC000h       row 4      I-1
+            LHI		R23,0x000F            ; set R23 = 0x000F0000h       row 5      II-1
+            ADDUI	R5,R23,0x7C00         ; set R5  = 0x000F7C00h       row 5      II-1
+            ADDUI	R6,R23,0xBD08         ; set R6  = 0x000FBD08h       row 6      III-1
+            LHI		R23,0x0006            ; set R23 = 0x00060000h       row 7      2-II
+            ADDUI	R7,R23,0xDFDB         ; set R7  = 0x0006DFDBh       row 7      2-II
+            ADDUI	R8,R23,0xEFDB         ; set R8  = 0x0006EFDBh       row 8      2-III
+            ADDUI	R9,R23,0xF400         ; set R9  = 0x0006F400h       row 9      II-2
+            ADDUI	R10,R23,0xF908        ; set R10 = 0x0006F908h       row 10     III-2
+            ADDUI	R11,R23,0xB9DB        ; set R11 = 0x0006B9DBh       row 11     3-II
+            ADDUI	R12,R23,0x76DB        ; set R12 = 0x000676DBh       row 12     3-III
+            ADDUI	R13,R23,0x135B        ; set R13 = 0x0006135Bh       row 13     II-3
+            ADDUI	R14,R23,0x339B        ; set R14 = 0x0006339Bh       row 14     III-3
+            LHI		R23,0x0009            ; set R21 = 0x00090000h       row 15     4-I
+            ADDUI	R15,R23,0x001F        ; set R15 = 0x0009001Fh       row 15     4-I
+            LHI		R23,0x0006            ; set R21 = 0x00060000h       row 16     4-II
+            ADDUI	R16,R23,0xBBEF        ; set R16 = 0x0006BBEFh       row 16     4-II
+            ADDUI	R17,R23,0x77F7        ; set R17 = 0x000677F7h       row 17     4-III
+            ADDUI	R18,R0,0x003B         ; set R18 = 0x0000003Bh       row 18     I-4
+            LHI		R23,0x0002            ; set R23 = 0x00020000h       row 19     II-4
+            ADDUI	R19,R23,0x13FD        ; set R19 = 0x000213FDh       row 19     II-4
+            LHI		R23,0x0006            ; set R23 = 0x00060000h       row 20     III-4
+            ADDUI	R20,R23,0x33FE        ; set R20 = 0x000633FE        row 20     III-4
                                               ; end of storage
       	    SB		R0,0x0001(R21)        ; CS_WRITE_0_STARTUP
                                               ; end of preamble
